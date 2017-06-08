eab5be8dda35713eaaf57667a1fa2e5245c319a7
somename <someemail>
2017-06-08 20:16:35 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -32,6 +32,6 @@ public class TestPlanetExplorer {
 		PlanetExplorer rover = new PlanetExplorer(3,3,null);
 		rover.moveLeft();
 		assertEquals("Vozilo menja smer kretanja na levo",
-					 "(2, 3, N)", rover.getFormatedCoorditates());
+					 "(3, 3, E)", rover.getFormatedCoorditates());
 	}
 }
